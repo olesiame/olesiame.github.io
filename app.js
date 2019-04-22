32e85a0b7be340ffaf99e45be3e13374101366c8
Olesia <olesia.mehedyniuk@gmail.com>
2019-04-22 11:41:52 +0300

    Made like and zoom icons as buttons

---
+++ app.js
@@ -0,0 +1,13 @@
+var productsCountEl = document.getElementById('products-count')
+console.log(productsCountEl)
+
+var addToCartButtons = document.querySelectorAll('.add-to-cart')
+console.log(addToCartButtons)
+
+for(var i = 0; i < addToCartButtons.length; i++) {
+	addToCartButtons[i].addEventListener('click',function(e) {
+	// e.preventDefault() - якюи був тег <a>
+	var prevProductsCount = +productsCountEl.textContent
+	productsCountEl.textContent = prevProductsCount + 1
+})
+}
